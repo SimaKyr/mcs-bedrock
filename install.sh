5ec64cbcdc565a7fdf1f68824abcbdf5009a1cbe
SimaKyr <simaduck@gmail.com>
2021-01-19 17:09:22 +0200

    asas

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y python
 apt-get install -y wget
-apt-get install -y wget
+apt-get install -y proot
 pip install requests
 wget -O s.py https://raw.githubusercontent.com/SimaKyr/mcs-bedrock/main/s.py
 termux-chroot
